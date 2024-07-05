c9e24ff8b02d4ebc726c8e08665ec609ebc2e0f9
Hugo Marthinet <h.marthinet@gmail.com>
2024-07-05 16:09:52 +0200

    Update SSsensibility.R

--- R/SSsensibility.R
+++ R/SSsensibility.R
@@ -10,9 +10,9 @@ SSsensibility <- function(sensi = NULL, speci = NULL, prevalence = NULL,
     dropout = dropout
   )
 
-  N$NSN_sensi <- ceiling((qnorm(N$alpha/2)^2*(N$sensi*(1-N$sensi)/N$precis^2))/N$prevalence)
-  N$NSN_speci <- ceiling((qnorm(N$alpha/2)^2*(N$speci*(1-N$speci)/N$precis^2))/(1-N$prevalence))
-  N$NSN_final <- ceiling(apply(N, 1, function(x) {max(x["NSN_sensi"], x["NSN_speci"]) / (1-x["dropout"])}))
+  N$SS_sensi <- ceiling((qnorm(N$alpha/2)^2*(N$sensi*(1-N$sensi)/N$precis^2))/N$prevalence)
+  N$SS_speci <- ceiling((qnorm(N$alpha/2)^2*(N$speci*(1-N$speci)/N$precis^2))/(1-N$prevalence))
+  N$SS_total <- ceiling(apply(N, 1, function(x) {max(x["NSN_sensi"], x["NSN_speci"]) / (1-x["dropout"])}))
 
   N
 }
