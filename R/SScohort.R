14eea6c3cc3c3e0454d95cede22c63723618998a
Hugo Marthinet <h.marthinet@gmail.com>
2024-09-17 10:04:49 +0200

    Translation

--- R/SScohort.R
+++ R/SScohort.R
@@ -1,38 +1,38 @@
-SScohort <- function(incidence.exp, incidence.non.exp = NULL,
-                        prop.traitement, fact.risque = NULL,
-                        temps.etude,
+SScohort <- function(incidence.exp, incidence.nexp = NULL,
+                        prop.treatment, risk.factor = NULL,
+                        time,
                         alpha = 0.05, power = 0.80) {
 
   #### Check Params ####
   if(is.null(incidence.exp)){
     stop("incidence.exp missing")
   }
-  if(is.null(prop.traitement)){
-    stop("prop.traitement missing")
+  if(is.null(prop.treatment)){
+    stop("prop.treatment missing")
   }
 
   #### Code Fonction ####
 
   #Gestion cas incidence non exp vs facteur de risque
-  if (is.null(incidence.non.exp)) {
-    incidence.non.exp <- incidence.exp * fact.risque
+  if (is.null(incidence.nexp)) {
+    incidence.nexp <- incidence.exp * risk.factor
   }
 
   #Calcul des paramètres
-  d <- abs(incidence.exp - incidence.non.exp)
-  gamma <- (1 - prop.traitement)/prop.traitement
-  mu <- (incidence.exp + gamma * incidence.non.exp) / (1 + gamma)
+  d <- abs(incidence.exp - incidence.nexp)
+  gamma <- (1 - prop.treatment)/prop.treatment
+  mu <- (incidence.exp + gamma * incidence.nexp) / (1 + gamma)
   z_alpha <- abs(qnorm(p = alpha/2))
   z_beta <- abs(qnorm(p = power))
 
   #Calcul du NSN
   N.exp <- (z_alpha * sqrt((1 + gamma) * mu * (1 - mu)) +
-              z_beta * sqrt((1 + incidence.exp) * incidence.non.exp * (1 - incidence.non.exp)))^2 /
+              z_beta * sqrt((1 + incidence.exp) * incidence.nexp * (1 - incidence.nexp)))^2 /
     (gamma * d^2)
 
   N.non.exp <- gamma * N.exp
   N.temps <- N.exp + N.non.exp
-  N <- N.temps / temps.etude
+  N <- N.temps / time
 
   return(list(N.exp = ceiling(N.exp),
               N.nexp = ceiling(N.non.exp),
@@ -40,5 +40,5 @@ SScohort <- function(incidence.exp, incidence.non.exp = NULL,
               N = ceiling(N)))
 }
 
-# SScohort(incidence.exp = 2/1000, fact.risque = 3,
-#             prop.traitement = 0.3, temps.etude = 5)
+# SScohort(incidence.exp = 2/1000, risk.factor = 3,
+#             prop.traitement = 0.3, time = 5)
